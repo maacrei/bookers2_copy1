6987e42e39286219794bbfd84c131dfa687516aa
EC2 Default User <ec2-user@ip-172-31-44-157.ec2.internal>
2022-02-12 06:40:21 +0000

    [Add]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  def index
+  end
+  
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
